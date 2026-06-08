004d37e0653fcb3863e147322fa0e0e51b1d5ed5
Peter Adel <99246822+peteradel22@users.noreply.github.com>
2026-06-08 14:53:06 +0300

    Update variables.tf

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,3 +1,23 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "instance_profile_name" {
+  description = "IAM instance profile name"
+  type        = string
+}
+
 variable "public_key" {
   description = "SSH public key for EC2 key pair"
   type        = string
