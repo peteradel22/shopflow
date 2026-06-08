725f97c779452b78e828e257c9b1a1109ec1c048
Peter Adel <99246822+peteradel22@users.noreply.github.com>
2026-06-08 14:45:02 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "public_key" {
   description = "SSH public key for EC2 key pair"
   type        = string
 }
+
+variable "image_tag" {
+  description = "Docker image tag injected by CI pipeline"
+  type        = string
+}
+
+variable "ecr_registry" {
+  description = "ECR registry URL injected by CI pipeline"
+  type        = string
+}
