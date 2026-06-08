891feef77dd7e2321dc415f81525e941175598de
Peter Adel <99246822+peteradel22@users.noreply.github.com>
2026-06-08 15:55:13 +0300

    Update userdata.sh

--- terraform/modules/ec2/userdata.sh
+++ terraform/modules/ec2/userdata.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 040768516109.dkr.ecr.us-east-1.amazonaws.com
+
+dnf install -y docker
+
+systemctl enable docker
+systemctl start docker
+
+aws ecr get-login-password --region us-east-1 | \
+docker login --username AWS --password-stdin \
+040768516109.dkr.ecr.us-east-1.amazonaws.com
+
 docker pull 040768516109.dkr.ecr.us-east-1.amazonaws.com/shopflow-app:latest
-docker run -d -p 3000:3000 040768516109.dkr.ecr.us-east-1.amazonaws.com/shopflow-app:latest
+
+docker run -d --restart always \
+-p 3000:3000 \
+040768516109.dkr.ecr.us-east-1.amazonaws.com/shopflow-app:latest
