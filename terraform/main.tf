98d8f73862352cdee8e811f64a49c490165cf451
Peter Adel <99246822+peteradel22@users.noreply.github.com>
2026-06-08 14:44:01 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,20 +7,19 @@ module "iam" {
 }
 
 module "ec2" {
-  source = "./modules/ec2"
-
-  vpc_id             = module.vpc.vpc_id
-  public_subnet_ids  = module.vpc.public_subnet_ids
-  private_subnet_ids = module.vpc.private_subnet_ids
-
+  source                = "./modules/ec2"
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_ids     = module.vpc.public_subnet_ids
+  private_subnet_ids    = module.vpc.private_subnet_ids
   instance_profile_name = module.iam.instance_profile_name
+  public_key            = var.public_key
+  image_tag             = var.image_tag
+  ecr_registry          = var.ecr_registry
 }
 
 module "rds" {
-  source = "./modules/rds"
-
+  source             = "./modules/rds"
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
-
-  ec2_sg_id = module.ec2.ec2_sg_id
+  ec2_sg_id          = module.ec2.ec2_sg_id
 }
